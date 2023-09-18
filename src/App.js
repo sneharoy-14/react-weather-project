590508a452d7365e747d04492133e6a4be4483cd
sneharoy-14 <sneharoy3104@gmail.com>
2023-09-18 11:46:53 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        <a
-          href="https://github.com/sneharoy-14/react-weather-project"
-          target="_blank"
-        >
-          Open-source code{" "}
-        </a>
-        by Sneha Dey Roy
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          <a
+            href="https://github.com/sneharoy-14/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by Sneha Dey Roy
+        </footer>
+      </div>
     </div>
   );
 }
