637f18737d94496de043139a697eaf50d8aef06e
sneharoy-14 <sneharoy3104@gmail.com>
2023-09-18 11:21:34 +0100

    Added the GitHub Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        <a
+          href="https://github.com/sneharoy-14/react-weather-project"
+          target="_blank"
+        >
+          Open-source code{" "}
+        </a>
+        by Sneha Dey Roy
+      </footer>
+    </div>
+  );
 }
