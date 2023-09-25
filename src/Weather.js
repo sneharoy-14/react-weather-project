6148618007ae787fdae3acffc9a00f8f5174e070
sneharoy-14 <sneharoy3104@gmail.com>
2023-09-25 14:39:48 +0100

    Added the Forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -62,7 +62,7 @@ export default function Weather(props) {
             </div>
           </form>
           <WeatherInfo data={weatherData} />
-          <WeatherForecast />
+          <WeatherForecast city={weatherData.city} />
         </div>
       </div>
     );
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,22 @@
+import axios from "axios";
 import React from "react";
+import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+  let apiKey = "babc1213f11b9atf604b57efa38oa64c";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="weatherForecast">
       <div className="row">
         <div className="col-2">
-          <div className="WeatherForecast-day">Monday</div>
+          <div className="WeatherForecast-day">Mon</div>
           <img
             src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
             alt="forecast-icon"
@@ -13,7 +24,7 @@ export default function WeatherForecast() {
           />
 
           <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">23° {"  "}</span>
+            <span className="WeatherForecast-temperature-max">23°</span>
             <span className="WeatherForecast-temperature-min">20°</span>
           </div>
         </div>
